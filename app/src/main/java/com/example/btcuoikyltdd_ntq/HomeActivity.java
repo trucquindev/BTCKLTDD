b4e6f094cf73afc7fe2f8ae9771365cb602bc689
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-11-27 07:53:09 +0700

    update

--- app/src/main/java/com/example/btcuoikyltdd_ntq/HomeActivity.java
+++ app/src/main/java/com/example/btcuoikyltdd_ntq/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.btcuoikyltdd_ntq;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
-
+    Button btnok;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class HomeActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnok = findViewById(R.id.okButton);
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
